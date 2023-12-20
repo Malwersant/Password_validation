fca321b5ca4db6d9c8a404e13f77283d61fcfb59
Malwersant <szymon.zochniak@gmail.com>
2023-12-20 21:48:14 +0100

    Check if user password is equal to password on created in app.

---
+++ main.py
@@ -0,0 +1,13 @@
+password = 'Herbitusin2019$'
+user_password = input('Proszę o podanie hasła: ')
+
+while user_password != password:
+    user_password = input('Hasło nieprawidłowe. Proszę o ponowne podanie hasła: ')
+
+print('Gratulacje, podałeś prawidłowe hasło.')
+
+
+
+
+
+
