98df0995e437636566724b56b89d3ff50514109e
Malwersant <szymon.zochniak@gmail.com>
2023-12-22 21:49:51 +0100

    Check if user password is equal to password created in app. Extend feature if user submit three times incorrect password.

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 password = 'kotas'
 user_password = input('Proszę o podanie hasła: ')
-# count = 1
 
 if user_password == password:
     print('Gratulacje, PIN jest poprawny!')
 else:
-    count = 2
-    user_password = input(f'Hasło nieprawidłowe. Zostało ci jeszcze {count} prób. Proszę o ponowne podanie hasła: ')
+    count = 1
     while count < 3:
+        user_password = input(f'Hasło nieprawidłowe. Zostało ci jeszcze {3 - count} prób. Proszę o ponowne podanie hasła: ')
         if user_password == password:
             print('Gratulacje, PIN jest poprawny!')
             break
-
-        else:
-            user_password = input(f'Hasło nieprawidłowe. Zostało ci jeszcze {count - 1} prób. Proszę o ponowne podanie hasła: ')
         count += 1
-        print('Podałeś 3 razy nierprawidłowe hasło. Karta została zablokowana.')
 
-#
-# Jeszcze nie działa przejście z drugiiego na trzeci licznik
-# Mimo podania prawidłowego hasła w trzeciej próbie i tak dostaję komunikat, że  razy nieprawidłowe hasło podane i karta zablokowana.
+    if count == 3:
+        print('Podałeś 3 razy nierprawidłowe hasło. Karta została zablokowana.')
